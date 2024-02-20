31d303557e31cb48ae50388a3cc5e81870e5c60c
Sarit Galanos <saritgalanos@gmail.com>
2024-02-20 15:36:21 +0200

    new build

--- mb-frontend/src/pages/UserIndex.jsx
+++ mb-frontend/src/pages/UserIndex.jsx
@@ -1,11 +1,12 @@
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
-import { useCallback, useState } from 'react'
+import { useCallback, useContext, useState } from 'react'
 import { useEffect } from 'react'
 import { utilService } from '../services/util.service.js'
 import { userService } from '../services/user.service.js'
 import { UserList } from '../cmps/UserList.jsx'
 import { UserFilter } from '../cmps/UserFilter.jsx'
 import { bugService } from '../services/bug.service.js'
+import { UserContext } from '../contexts/UserContext.js'
 
 
 export function UserIndex() {
