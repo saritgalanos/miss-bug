95715dd41301b307b4f1edfb6cf6cab7aac38431
Sarit Galanos <saritgalanos@gmail.com>
2024-02-25 19:55:15 +0200

    mongo update

--- mb-backend/config/prod.js
+++ mb-backend/config/prod.js
@@ -1,4 +1,11 @@
+// export default {
+//     dbURL: 'mongodb+srv://saritgalanos:Ronnie@mycluster.ybtt9j6.mongodb.net/?retryWrites=true&w=majority',
+//     dbName: 'miss_bug'
+// }
+
 export default {
-    dbURL: 'mongodb+srv://saritgalanos:Ronnie@mycluster.ybtt9j6.mongodb.net/miss_bug?retryWrites=true&w=majority',
+    //dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://saritgalanos:Ronnie@mycluster.ybtt9j6.mongodb.net/?retryWrites=true&w=majority',
+    //dbURL: 'mongodb+srv://tommyirmia:66eZpBwlYR5bPxOi@mycluster.hvdhdxf.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'miss_bug'
 }
